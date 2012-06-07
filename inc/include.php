898ee4d5cdf14588ef76c813cba53fe842247ac7
Frank Fishburn <frankthefishburn@gmail.com>
2012-06-07 01:44:26 -0400

    Added functions for directory size and formatting

--- inc/include.php
+++ inc/include.php
@@ -113,4 +113,39 @@ function findwebfile($path) {
     return $filename;
 }
 
+function foldersize($path) {
+    $total_size = 0;
+    $files = scandir($path);
+    $cleanPath = rtrim($path, '/'). '/';
+
+    foreach($files as $t) {
+        if ($t<>"." && $t<>"..") {
+            $currentFile = $cleanPath . $t;
+            if (is_dir($currentFile)) {
+                $size = foldersize($currentFile);
+                $total_size += $size;
+            }
+            else {
+                $size = filesize($currentFile);
+                $total_size += $size;
+            }
+        }   
+    }
+
+    return $total_size;
+}
+
+function format_size($size) {
+    $units = explode(' ', 'B KB MB GB TB PB');
+
+    $mod = 1024;
+
+    for ($i = 0; $size > $mod; $i++) {
+        $size /= $mod;
+    }
+
+    $endIndex = strpos($size, ".")+3;
+
+    return substr( $size, 0, $endIndex).' '.$units[$i];
+}
 ?>
