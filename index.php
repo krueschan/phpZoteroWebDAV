8cad6bfcf567bccca5c07cb62d3515194ee0f3a4
ddean4040 <david@generalthreat.com>
2012-11-24 04:23:45 +0000

    Add support for Collections

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $ipp  = isset($_REQUEST['ipp']) ? (int)$_REQUEST['ipp'] : $def_ipp;
 $sort = isset($_REQUEST['sort']) ? $_REQUEST['sort'] : $def_sort; 
 $sortorder = isset($_REQUEST['sortorder']) ? $_REQUEST['sortorder'] : $def_sortorder;
 $page = isset($_REQUEST['page']) ? (int)$_REQUEST['page'] : 1;
+$collectionKey = isset( $_REQUEST['collection'] ) ? $_REQUEST['collection'] : false;
 
 $webdav_url=( isset($_SERVER['HTTPS']) ? 'https' : 'http') . "://" . $_SERVER['HTTP_HOST']  . str_replace('?'.$_SERVER['QUERY_STRING'],'',$_SERVER['REQUEST_URI']) . "webdav_server.php/zotero/";
 
@@ -37,18 +38,43 @@ purge_cache(realpath("./" . $cache_dir), $cache_age);
 $start = ($page - 1) * $ipp;
 if ($ipp > $fetchlimit) $limit = $fetchlimit; else $limit = $ipp;
 
-// TODO: include collections on index page for traversal
-//$collections = $zotero->fetchCollections( array( 'collectionKey' => '' ) );
+// Include collections on index page for traversal
+$collections = $zotero->fetchCollections( array( 'collectionKey' => $collectionKey ) );
 //print_r($collections);
-//if( count( $collections ) ) {
-//	$collectionKey = $collections[0]->collectionKey;
-//}
+
+?>
+<div class="collections-div">
+<h2><?php echo ( $collectionKey ) ? 'Sub-Collections' : 'Collections' ?></h2>
+<?php if( count( $collections ) > 0 ) : ?>
+<table>
+    <thead>
+        <tr>
+            <th scope="col">Collection</th>
+            <th scope="col"># Items</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach( $collections as $collection ) : ?>
+	<?php if( $collection->parentCollectionKey != $collectionKey ) continue; ?>
+        <tr>
+            <td><a href="?collection=<?php echo $collection->collectionKey; ?>"><?php echo $collection->name; ?></a></td>
+            <td><?php echo $collection->numItems; ?> <?php echo ( $collection->numItems == 1 ) ? 'item' : 'items' ; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php else : ?>
+<p class="note">No collections were found.</p>
+<?php endif; ?>
+</div>
+<?php
 
 $fetch_params = array(
     'order'   => $sort,
     'sort'    => $sortorder,
 //    'content' => 'none',
-    'limit'   => $limit
+    'limit'   => $limit,
+    'collectionKey' => $collectionKey
 );
 
 $fetch_offset = $start;
@@ -74,7 +100,10 @@ $totalitems = $zotero->getLastFeed()->totalResults;
 // MAIN DATA TABLE
 // parse result sets, write out data and get more from API if needed
 ?>
+<div class="all-items-div">
+<h2><?php echo ( $collectionKey ) ? 'Items in this Collection' : 'All Items'; ?></h2>
 <table class="library-items-div">
+    <thead>
     <tr>
         <th>Attachments</th>
         <th><a href="?page=1&sort=dateAdded">Added</a></th>
@@ -82,6 +111,8 @@ $totalitems = $zotero->getLastFeed()->totalResults;
         <th><a href="?page=1&sort=date&sortorder=<?php echo $orders[!(boolean) abs(strcmp($sort,"date"))] ?>">Date</a></th>
         <th><a href="?page=1&sort=title&sortorder=<?php echo $orders[!(boolean) abs(strcmp($sort,"title"))] ?>">Title</a></th>
     </tr>
+    </thead>
+    <tbody>
 <?php foreach( $items as $item ) { ?>
     <tr>
         <td><a href="details.php?itemkey=<?php echo $item->itemKey ?>"><?php echo $item->numChildren ?></a></td>
@@ -91,27 +122,30 @@ $totalitems = $zotero->getLastFeed()->totalResults;
         <td><a href="details.php?itemkey=<?php echo $item->itemKey ?>"><?php echo $item->title ?></a></td>
     </tr>
 <?php } ?>
+    </tbody>
     <tfoot>
         <tr>
             <td><?php echo(($start +1) . " to " . count( $items ) . " of " . $totalitems); ?></td>
         </tr>
     </tfoot>
 </table>
+</div>
 <br />
 <hr />
 <table>
 <?php
 
 // NAVIGATION FOOTER
-$parm_ipp = ($ipp == $def_ipp) ? "": "&ipp=$ipp";
-$parm_sort = ($sort == $def_sort) ? "": "&sort=$sort";
-$parm_sortorder = ($sortorder == $def_sortorder) ? "": "&sortorder=$sortorder";
+$param_ipp = ($ipp == $def_ipp) ? "": "&ipp=$ipp";
+$param_sort = ($sort == $def_sort) ? "": "&sort=$sort";
+$param_sortorder = ($sortorder == $def_sortorder) ? "": "&sortorder=$sortorder";
+$param_collection = ( $collectionKey ) ? "&collection=$collectionKey" : '';
 $i = 1;
 echo("<tr><td>Pages</td><td>");
 $pages = intval($totalitems / $ipp) + 1;
 
 while ($i <= $pages) {
-    if ($i != $page) echo("<a href=\"?page=$i" . $parm_ipp . $parm_sort . $parm_sortorder . "\">");
+    if ($i != $page) echo("<a href=\"?page=$i" . $param_ipp . $param_sort . $param_sortorder . $param_collection . "\">");
     echo ("-$i-");
     if ($i != $page) echo("</a>");
     echo ("&nbsp;&nbsp;&nbsp;");
@@ -134,7 +168,7 @@ echo("<tr><td>Items&nbsp;per&nbsp;Page</td><td>");
 $ipp_list = array (1,10,20,50,100,200,500,1000,9999999);
 $i = 0;
 while ($i <= 7) {
-    if ($ipp != $ipp_list[$i]) echo("<a href=\"?page=$page&ipp=" . $ipp_list[$i] . $parm_sort . $parm_sortorder . "\">");
+    if ($ipp != $ipp_list[$i]) echo("<a href=\"?page=$page&ipp=" . $ipp_list[$i] . $param_sort . $param_sortorder . $param_collection . "\">");
     echo ("-$ipp_list[$i]-");
     if ($ipp != $ipp_list[$i]) echo("</a>");
     echo ("&nbsp;&nbsp;&nbsp;");
@@ -147,7 +181,7 @@ echo("<tr><td>Sort By</td><td>");
 $s_list = array ("dateAdded", "title", "creator", "type", "date", "publisher", "publication", "journalAbbreviation", "language", "dateModified", "accessDate", "libraryCatalog", "callNumber", "rights", "addedBy", "numItems");
 $i = 0;
 while ($i <= 15) {
-    if ($sort != $s_list[$i]) echo("<a href=\"?page=$page" . $parm_ipp . "&sort=" . $s_list[$i] . $parm_sortorder . "\">");
+    if ($sort != $s_list[$i]) echo("<a href=\"?page=$page" . $param_ipp . "&sort=" . $s_list[$i] . $param_sortorder . $param_collection . "\">");
     echo ("-$s_list[$i]-");
     if ($sort != $s_list[$i]) echo("</a>");
     echo ("&nbsp;&nbsp;&nbsp;");
@@ -158,7 +192,7 @@ echo("<tr><td>Sort Order</td><td>");
 $so_list = array ("asc", "desc");
 $i = 0;
 while ($i <= 1) {
-    if ($sortorder != $so_list[$i]) echo("<a href=\"?page=$page" . $parm_ipp . $parm_sort . "&sortorder=" . $so_list[$i] . "\">");
+    if ($sortorder != $so_list[$i]) echo("<a href=\"?page=$page" . $param_ipp . $param_sort . "&sortorder=" . $so_list[$i] . $param_collection . "\">");
     echo ("-$so_list[$i]-");
     if ($sortorder != $so_list[$i]) echo("</a>");
     echo ("&nbsp;&nbsp;&nbsp;");
