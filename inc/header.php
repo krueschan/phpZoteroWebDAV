4d58220da01063544a84de94ab961d036cf33fe3
ddean4040 <david@generalthreat.com>
2012-11-24 04:17:25 +0000

    Add charset declaration to fix occasional character bugs

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8" />
     <title>phpZoteroWebDAV</title>
     <link rel="stylesheet" href="inc/css.css" type="text/css" media="screen" charset="utf-8"/>
     <style type="text/css">
