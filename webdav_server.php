a055b032404042f546cd51be08209e26f95c0131
ddean4040 <david@generalthreat.com>
2013-05-30 12:21:46 +0000

    commit

--- webdav_server.php
+++ webdav_server.php
@@ -8,9 +8,10 @@ require 'include.php';
 require_once "HTTP/WebDAV/Server/Filesystem.php";
 $server = new HTTP_WebDAV_Server_Filesystem();
 
-error_log( dirname( get_real_path( $data_dir ) ) );
-error_log( dirname( dirname(__FILE__) . '/' . $data_dir ) );
+error_log( 'Function: ' . dirname( get_real_path( $data_dir ) ) );
+error_log( 'Manual: ' . dirname( dirname(__FILE__) . '/' . $data_dir ) );
 
-$server->ServeRequest( dirname( get_real_path( $data_dir ) ) );
+#$server->ServeRequest( dirname( get_real_path( $data_dir ) ) );
+$server->ServeRequest( dirname( dirname(__FILE__) . '/' . $data_dir ) );
 
 ?>
