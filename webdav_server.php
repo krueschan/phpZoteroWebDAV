a18a57acb66cf023172ef30b1b0f081686baf9dd
ddean4040 <david@generalthreat.com>
2013-05-29 22:47:47 -0400

    Make WebDAV server respect `$data_dir` setting - #1 thanks, dmathieu

--- webdav_server.php
+++ webdav_server.php
@@ -1,10 +1,12 @@
 <?php
 
+require 'settings.php';
+
 chdir (dirname(__FILE__) . "/inc");
 
 require_once "HTTP/WebDAV/Server/Filesystem.php";
 $server = new HTTP_WebDAV_Server_Filesystem();
 
-$server->ServeRequest(dirname(__FILE__) . "/data");
+$server->ServeRequest( dirname( dirname(__FILE__) . '/' . $data_dir ) );
 
 ?>
